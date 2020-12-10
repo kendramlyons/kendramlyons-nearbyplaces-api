c82abf4ee3ae54955f809936c437adad47558257
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-12-09 18:34:38 -0700

    connected api to front end

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const data = require('./data');
 
 const app = express();
@@ -7,13 +8,15 @@ const port = process.env.PORT || 3003;
 
 //middleware
 app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (request, response) => {
     response.send('Welcome to Nearby Places API');
 });
 
-// app.post('/place', (request, response) => {
+app.post('/place', (request, response) => {
 
-// });
+});
 app.get('/places', (request, response) => {
     let metadata = data.places.map(x => {
         return{name: x.name, reviews: x.reviews};
