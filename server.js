3dd81a312c03d30e1a146d59362a114795d0af22
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-12-11 14:31:12 -0700

    remove node modules

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const data = require('./data');
+const db = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3003;
@@ -17,11 +18,30 @@ app.get('/', (request, response) => {
 app.post('/place', (request, response) => {
 
 });
+function groupBy(collection, property) {
+    var i = 0, val, index,
+        values = [], result = [];
+    for (; i < collection.length; i++) {
+        val = collection[i][property];
+        index = values.indexOf(val);
+        if (index > -1)
+            result[index].push(collection[i]);
+        else {
+            values.push(val);
+            result.push([collection[i]]);
+        }
+    }
+    return result;
+}
 app.get('/places', (request, response) => {
-    let metadata = data.places.map(x => {
-        return{name: x.name, reviews: x.reviews};
-    });
-    response.json(metadata);
+    db.getPlaces().then(x => response.json(groupBy(x, 'placeid')))
+    .catch(e => response.status(500).json({error: e}));
+});
+app.post('/review/:placeId', (request, response) => {
+
+});
+app.get('/search/:searchTerm/:location', (request, response) => {
+
 });
 app.listen(port, () => {
     console.log(`Nearby Places API listening on port ${port}!`);
